30d1fbe0da6e035e103d198a76e17e21080d2c2a
Adriana Sanduta <adriana.sanduta@isa.utm.md>
2021-03-01 04:56:00 +0000

    fix conflict

--- lib/worker_supervised.ex
+++ lib/worker_supervised.ex
@@ -31,15 +31,12 @@ defmodule Worker do
     decoded_message["message"]["tweet"]["text"]
   end
 
-<<<<<<< HEAD
-=======
   defp make_analysis(values) do
     values
     |> Enum.reduce(0, fn key_value, coll -> EmotionScores.get_emotion(key_value) + coll end)
     |> Kernel./(length(values))
   end
 
->>>>>>> aab9a99... WIP
   def process_data(message) do
     if message.data =~ "panic" do
       IO.inspect("KILL MESSAGE HERE")
@@ -47,35 +44,14 @@ defmodule Worker do
       rm_characters = [",", ":", "?", ".", "!"]
       text = read_json(message)
       |> String.replace(rm_characters, "")
-<<<<<<< HEAD
-      |> String.split(" ", trim: true)
-      MyIO.my_inspect(%{"RECEIVED: " => text})
 
-      analyzed_text = make_analysis(text)
-      MyIO.my_inspect(%{"SENTIMENT SCORE: " => analyzed_text})
-      MyIO.my_inspect("================================================================================")
-    end
-  end
-
-  defp make_analysis(values) do
-    values
-    |> Enum.reduce(0, fn key_value, coll -> EmotionValues.get_emotion(key_value) + coll end)
-    |> Kernel./(length(values))
-  end
-
-=======
       score = make_analysis(text |> String.split(" ", trim: true))
       log("#{score} ==> #{text}")
     end
   end
 
-<<<<<<< HEAD
->>>>>>> aab9a99... WIP
-=======
   def log(data) do
     data |> inspect() |> IO.puts()
     data
   end
-
->>>>>>> 138e10d... remove getMessage
 end
